703cda0e37ca4afb789736a6c4b50d543101cba9
AnastasiaDzam <yorklo@mail.ru>
2023-06-05 21:13:12 +0500

    added router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+<<<<<<< HEAD
 import App from './App.jsx'
 import './assets/styles/index.scss'
 
@@ -7,4 +8,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+=======
+
+import './assets/styles/index.scss'
+import Router from './routes/Routes'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<Router />
+	</React.StrictMode>
+>>>>>>> 6ba7f4a (added router dom)
 )
